84008e4ca1f024f562b326b96399c9c342749b26
Vedat ERMIS <vedatermis@hotmail.com>
2020-10-18 17:58:36 +0300

    commit

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/home.dart';
+import 'package:fluttershare/widgets/progress.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -7,17 +11,35 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  TextEditingController textEditingController = new TextEditingController();
+  Future<QuerySnapshot> searchResultsFuture;
+
+  void clearSearchText() {
+    textEditingController.clear();
+  }
+
+  void handleSearch(String value) {
+    Future<QuerySnapshot> users =
+        usersRef.where("displayName", isGreaterThanOrEqualTo: value).get();
+
+    setState(() {
+      searchResultsFuture = users;
+    });
+  }
+
   AppBar buildSearchField() {
     return AppBar(
       backgroundColor: Colors.white,
       title: TextFormField(
+        controller: textEditingController,
+        onFieldSubmitted: handleSearch,
         decoration: InputDecoration(
             hintText: "Search for a user...",
             filled: true,
             prefixIcon: Icon(Icons.account_box, size: 28),
             suffixIcon: IconButton(
               icon: Icon(Icons.clear),
-              onPressed: () => print("clear"),
+              onPressed: clearSearchText,
             )),
       ),
     );
@@ -50,12 +72,33 @@ class _SearchState extends State<Search> {
     );
   }
 
+  FutureBuilder buildSearchResults() {
+    return FutureBuilder(
+        future: searchResultsFuture,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return circularProgress();
+          }
+
+          List<Text> searchResults = [];
+          snapshot.data.documents.forEach((doc) {
+            User user = User.fromDocument(doc);
+            searchResults.add(Text(user.displayName));
+          });
+
+          return ListView(
+            children: searchResults,
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
       appBar: buildSearchField(),
-      body: buildNoContent(),
+      body:
+          searchResultsFuture == null ? buildNoContent() : buildSearchResults(),
     );
   }
 }
